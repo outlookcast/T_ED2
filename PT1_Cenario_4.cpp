7e2cfb6523f12fb3b97ce22aa432937f7702ecc9
Vinicius <vsampaio@ice.ufjf.br>
2018-04-15 14:10:22 -0300

    inserindo testes da HasEncad

--- PT1_Cenario_4.cpp
+++ PT1_Cenario_4.cpp
@@ -20,65 +20,62 @@ void PT1_Cenario_4()
 {
 
     Data * vetorDeDados = readFile();
+    Data * vetor1000 =  vetorRandomData(1000, vetorDeDados);
+    Data * vetor5000 = vetorRandomData(5000, vetorDeDados);
+    Data * vetor10000 = vetorRandomData(10000, vetorDeDados);
+    Data * vetor50000 = vetorRandomData(50000, vetorDeDados);
+    Data * vetor100000 = vetorRandomData(100000, vetorDeDados);
+    Data * vetor500000 = vetorRandomData(500000, vetorDeDados);
 
+    ///  ---------------------------------------- INICIO DOS TESTES PARA TRATAMENTO LINEAR ---------------------------------------- ///
+    cout<<"///  -------- INICIO DOS TESTES PARA TRATAMENTO LINEAR -------- ///"<<endl;
 
     ///Testando para N=1.000 -> Hash Linear
-    Data * vetor1000 =  vetorRandomData(1000, vetorDeDados);
     HashInt * hashLinear1000 = new HashInt(1000,1);
     for(int i=0;i<1000;i++)
     {
         hashLinear1000->inserir(vetor1000[i].getQuestionID());
     }
     cout<<"Numero de colisoes (Tratamento linear N=1.000): "<<hashLinear1000->getNumColisoes()<<endl;
-    delete [] vetor1000;
     delete hashLinear1000;
 
     ///Testando para N=5.000 -> Hash Linear
-    Data * vetor5000 = vetorRandomData(5000, vetorDeDados);
     HashInt * hashLinear5000 = new HashInt(5000,1);
     for(int i=0;i<5000;i++)
     {
         hashLinear5000->inserir(vetor5000[i].getQuestionID());
     }
     cout<<"Numero de colisoes (Tratamento linear N=5.000): "<<hashLinear5000->getNumColisoes()<<endl;
-    delete [] vetor5000;
     delete hashLinear5000;
 
     ///Testando para N=10.000 -> Hash Linear
-    Data * vetor10000 = vetorRandomData(10000, vetorDeDados);
     HashInt * hashLinear10000 = new HashInt(10000,1);
     for(int i=0;i<10000;i++)
     {
         hashLinear10000->inserir(vetor10000[i].getQuestionID());
     }
     cout<<"Numero de colisoes (Tratamento linear N=10.000): "<<hashLinear10000->getNumColisoes()<<endl;
-    delete [] vetor10000;
     delete hashLinear10000;
 
     ///Testando para N=50.000 -> Hash Linear
-    Data * vetor50000 = vetorRandomData(50000, vetorDeDados);
     HashInt * hashLinear50000 = new HashInt(50000,1);
     for(int i=0;i<50000;i++)
     {
         hashLinear50000->inserir(vetor50000[i].getQuestionID());
     }
     cout<<"Numero de colisoes (Tratamento linear N=50.000): "<<hashLinear50000->getNumColisoes()<<endl;
-    delete [] vetor50000;
     delete hashLinear50000;
 
     ///Testando para N=100.000 -> Hash Linear
-    Data * vetor100000 = vetorRandomData(100000, vetorDeDados);
     HashInt * hashLinear100000 = new HashInt(100000,1);
     for(int i=0;i<100000;i++)
     {
         hashLinear100000->inserir(vetor100000[i].getQuestionID());
     }
     cout<<"Numero de colisoes (Tratamento linear N=100.000): "<<hashLinear100000->getNumColisoes()<<endl;
-    delete [] vetor100000;
     delete hashLinear100000;
 
     ///Testando para N=500.000 -> Hash Linear
-    Data * vetor500000 = vetorRandomData(500000, vetorDeDados);
     HashInt * hashLinear500000 = new HashInt(500000,1);
     for(int i=0;i<500000;i++)
     {
@@ -86,8 +83,90 @@ void PT1_Cenario_4()
     }
     cout<<"Numero de colisoes (Tratamento linear N=500.000): "<<hashLinear500000->getNumColisoes()<<endl;
     delete hashLinear500000;
-    delete [] vetor500000;
 
+    ///  ---------------------------------------- FIM DOS TESTES PARA TRATAMENTO LINEAR ---------------------------------------- ///
+    cout<<"///  -------- FIM DOS TESTES PARA TRATAMENTO LINEAR -------- ///"<<endl;
+
+    cout<<endl;
+    cout<<endl;
+    cout<<endl;
+
+    ///  ---------------------------------------- INICIO DOS TESTES PARA TRATAMENTO ENCADEAMENTO SEPARADO ---------------------------------------- ///
+    cout<<"///  -------- INICIO DOS TESTES PARA TRATAMENTO ENCADEAMENTO SEPARADO -------- ///"<<endl;
+
+    ///Testando para N=1.000 -> ENCADEAMENTO SEPARADO
+    HashEncad * hashHashEncad1000 = new HashEncad(1000);
+    for(int i=0;i<1000;i++)
+    {
+        hashHashEncad1000->inserir(vetor1000[i].getQuestionID());
+    }
+    cout<<"Numero de colisoes (Tratamento linear N=1.000): "<<hashHashEncad1000->getNumColisoes()<<endl;
+    delete hashHashEncad1000;
+
+
+    ///Testando para N=5.000 -> ENCADEAMENTO SEPARADO
+    HashEncad * hashHashEncad5000 = new HashEncad(5000);
+    for(int i=0;i<5000;i++)
+    {
+        hashHashEncad5000->inserir(vetor5000[i].getQuestionID());
+    }
+    cout<<"Numero de colisoes (Tratamento linear N=5.000): "<<hashHashEncad5000->getNumColisoes()<<endl;
+    delete hashHashEncad5000;
+
+
+    ///Testando para N=10.000 -> ENCADEAMENTO SEPARADO
+    HashEncad * hashHashEncad10000 = new HashEncad(10000);
+    for(int i=0;i<10000;i++)
+    {
+        hashHashEncad10000->inserir(vetor10000[i].getQuestionID());
+    }
+    cout<<"Numero de colisoes (Tratamento linear N=10.000): "<<hashHashEncad10000->getNumColisoes()<<endl;
+    delete hashHashEncad10000;
+
+
+    ///Testando para N=50.000 -> ENCADEAMENTO SEPARADO
+    HashEncad * hashHashEncad50000 = new HashEncad(50000);
+    for(int i=0;i<50000;i++)
+    {
+        hashHashEncad50000->inserir(vetor50000[i].getQuestionID());
+    }
+    cout<<"Numero de colisoes (Tratamento linear N=50.000): "<<hashHashEncad50000->getNumColisoes()<<endl;
+    delete hashHashEncad50000;
+
+
+    ///Testando para N=100.000 -> ENCADEAMENTO SEPARADO
+    HashEncad * hashHashEncad100000 = new HashEncad(100000);
+    for(int i=0;i<100000;i++)
+    {
+        hashHashEncad100000->inserir(vetor100000[i].getQuestionID());
+    }
+    cout<<"Numero de colisoes (Tratamento linear N=100.000): "<<hashHashEncad100000->getNumColisoes()<<endl;
+    delete hashHashEncad100000;
+
+
+    ///Testando para N=500.000 -> ENCADEAMENTO SEPARADO
+    HashEncad * hashHashEncad500000 = new HashEncad(500000);
+    for(int i=0;i<500000;i++)
+    {
+        hashHashEncad500000->inserir(vetor500000[i].getQuestionID());
+    }
+    cout<<"Numero de colisoes (Tratamento linear N=500.000): "<<hashHashEncad500000->getNumColisoes()<<endl;
+    delete hashHashEncad500000;
+
+
+
+    ///  ---------------------------------------- FIM DOS TESTES PARA TRATAMENTO ENCADEAMENTO SEPARADO ---------------------------------------- ///
+    cout<<"///  -------- FIM DOS TESTES PARA TRATAMENTO ENCADEAMENTO SEPARADO -------- ///"<<endl;
+
+
+
+
+    delete [] vetor1000;
+    delete [] vetor5000;
+    delete [] vetor10000;
+    delete [] vetor50000;
+    delete [] vetor100000;
+    delete [] vetor500000;
 
     delete [] vetorDeDados;
 
