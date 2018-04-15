f8ac6781c92fa3f8f0cf7aaa07855edd105c0aca
almirph <phelipear@gmail.com>
2018-04-15 20:10:10 -0300

    quickSort3

--- PT1_Cenario_3.cpp
+++ PT1_Cenario_3.cpp
@@ -1,5 +1,26 @@
 #include "PT1_Cenario3.h"
+#include "stdlib.h"
+#include "time.h"
+#include "quickSort3.h"
 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+#include <vector>
+#include "Data.h"
+#include "Hash.h"
+#include <stdlib.h>
+#include "quickSort1.h"
+#include "leitura.h"
+#include "HashInt.h"
+#include "HashEncad.h"
+#include "No.h"
+#include "Tags.h"
+#include "answer.h"
+#include "leitura3.h"
+#include "PT1_Cenario3.h"
 void PT1_Cenario_3()
 {
 	Data *data = readFile();
@@ -18,15 +39,15 @@ void PT1_Cenario_3()
 
 	int comparacaoQuick = 0;
 	int trocaQuick = 0;
-	double tempoQuick = 0;	
+	double tempoQuick = 0;
 
 	int comparacaoInsert = 0;
 	int trocaInsert = 0;
 	double tempoInsert = 0;
-	
+
 	int trocaRadix = 0;
 	double tempoRadix = 0;
-	
+
 	for(int i = 0; i < 6; ++i)
 	{
 		comparacaoHeap = 0;
@@ -39,11 +60,11 @@ void PT1_Cenario_3()
 
 		comparacaoQuick = 0;
 		trocaQuick = 0;
-		tempoQuick = 0;	
+		tempoQuick = 0;
 
 		comparacaoInsert = 0;
 		trocaInsert = 0;
-		tempoInsert = 0;			
+		tempoInsert = 0;
 
 		trocaRadix = 0;
 		tempoRadix = 0;
@@ -53,33 +74,33 @@ void PT1_Cenario_3()
 		clock_t t2;
 
 		for(int j=0; j < 5; ++j)
-		{		
+		{
 			int * vetorRand = vetorRandomInt(vetor[i], data);
-			
-			for(int k = 0; k < vetor[i]; ++k)
-				vetorAux[k] = vetorRand [k];
-			
-			t1 = clock();
-			heapSort(vetorAux, vetor[i], &comparacaoHeap, &trocaHeap);
-			t2 = clock();
-			
-			tempoHeap += (t2 - t1) * 1000.0 / CLOCKS_PER_SEC;			
 
 			for(int k = 0; k < vetor[i]; ++k)
 				vetorAux[k] = vetorRand [k];
-			
+
 			t1 = clock();
-			//quicksort3(vetorAux, vetor[i], &comparacaoQuick, &trocaQuick);
+			heapSort(vetorAux, vetor[i], &comparacaoHeap, &trocaHeap);
+			t2 = clock();
+
+			tempoHeap += (t2 - t1) * 1000.0 / CLOCKS_PER_SEC;
+
+			for(int k = 0; k < vetor[i]; ++k)
+				vetorAux[k] = vetorRand [k];
+
+			t1 = clock();
+			quicksort3(vetorAux, vetor[i], &comparacaoQuick, &trocaQuick);
 			t2 = clock();
 
 			tempoQuick += (t2 - t1) * 1000.0 / CLOCKS_PER_SEC;
 
 			for(int k = 0; k < vetor[i]; ++k)
 				vetorAux[k] = vetorRand [k];
-			
+
 			t1 = clock();
-			//insertion(vetorAux, vetor[i], &comparacaoInsert, &trocaInsert);
-			t2 = clock();			
+			insertion(vetorAux, vetor[i], &comparacaoInsert, &trocaInsert);
+			t2 = clock();
 
 			tempoInsert += (t2 - t1) * 1000.0 / CLOCKS_PER_SEC;
 
@@ -90,19 +111,19 @@ void PT1_Cenario_3()
 			merge(vetorAux, vetor[i], &trocaMerge, &comparacaoMerge);
 			t2 = clock();
 
-			tempoMerge += (t2 - t1) * 1000.0 / CLOCKS_PER_SEC;			
+			tempoMerge += (t2 - t1) * 1000.0 / CLOCKS_PER_SEC;
 
 			for(int k = 0; k < vetor[i]; ++k)
 				vetorAux[k] = vetorRand [k];
-			
+
 			t1 = clock();
-			radixSort(vetorAux, vetor[i], &trocaRadix);	
+			radixSort(vetorAux, vetor[i], &trocaRadix);
 			t2 = clock();
 
 			tempoRadix += (t2 - t1) * 1000.0 / CLOCKS_PER_SEC;
 
-			delete []vetorRand;	
-			
+			delete []vetorRand;
+
 		}
 
 		comparacaoHeap /= 5;
@@ -115,15 +136,15 @@ void PT1_Cenario_3()
 
 		comparacaoQuick /= 5;
 		trocaQuick /= 5;
-		tempoQuick /= 5;	
+		tempoQuick /= 5;
 
 		comparacaoInsert /= 5;
 		trocaInsert /= 5;
-		tempoInsert /= 5;			
+		tempoInsert /= 5;
 
 		trocaRadix /= 5;
 		tempoRadix /= 5;
-		
+
 		saida<<"\nTam = "<<vetor[i]<<"\n\n    HeapSorte:\n     Numero de comparacoes Medio: "<<comparacaoHeap<<"\n     Numero de trocas Medio:"<<trocaHeap<<"\n     Tempo Medio"<<tempoHeap<<endl;
 		saida<<"\n\n    MergeSorte:\n     Numero de comparacoes Medio: "<<comparacaoMerge<<"\n     Numero de trocas Medio:"<<trocaMerge<<"\n     Tempo Medio"<<tempoMerge<<endl;
 		saida<<"\n\n    QuickSorte:\n     Numero de comparacoes Medio: "<<comparacaoQuick<<"\n     Numero de trocas Medio:"<<trocaQuick<<"\n     Tempo Medio"<<tempoQuick<<endl;
--- quickSort3.h
+++ quickSort3.h
@@ -1,10 +1,9 @@
 #include "quickSort2.h"
-#ifndef QUICKSORT3_H_INCLUDED
-#define QUICKSORT3_H_INCLUDED
+
 void quicksort3(int *vetor, int inicio, int fim, int *comparacoes, int *numCopias);
 void quicksort3(int *vetor, int tam, int *comparacoes, int *numCopias);
 void insertion(int *vetor, int n, int *comparacoes, int *numCopias);
 
 
 
-#endif
+
