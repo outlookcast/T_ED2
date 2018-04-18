0fd81542f1ffb9adfc2da184beee8cac3b1d0cd9
ICE <ice@lab01m11.nrc.ice.ufjf.br>
2018-04-18 18:26:01 -0300

    mudancas

--- quickSort1.h
+++ quickSort1.h
@@ -1,4 +1,6 @@
 #include "Data.h"
+#include "FrequenciaTag.h"
+
 void swap(int *valor1, int *valor2);
 void quicksort(int *vetor, int inicio, int fim, int *comparacoes, int *numCopias);
 void quicksort(int *vetor, int n, int *comparacoes, int *numCopias);
@@ -6,5 +8,6 @@ void quicksort(int *vetor, int n, int *comparacoes, int *numCopias);
 void quicksort(Data *vetor, int inicio, int fim, int *comparacoes, int *numCopias);
 void quicksort(Data *vetor, int n, int *comparacoes, int *numCopias);
 
-
-
+void quicksort(FrequnciaTag *vetor, int inicio, int fim);
+void quicksort(FrequnciaTag *vetor, int n);
+void swap(FrequnciaTag *valor1, FrequnciaTag *valor2);
