207d5c8751f503cbc2c191c08fb0e631b311de0d
almirph <phelipear@gmail.com>
2018-05-06 12:15:13 -0300

    Arrumando arquivos para o trabalho 2

--- quickSort3.h
+++
@@ -1,9 +0,0 @@
-#include "quickSort2.h"
-
-void quicksort3(int *vetor, int inicio, int fim, int *comparacoes, int *numCopias);
-void quicksort3(int *vetor, int tam, int *comparacoes, int *numCopias);
-void insertion(int *vetor, int n, int *comparacoes, int *numCopias);
-
-
-
-
--- radixSort.h
+++
@@ -1,6 +0,0 @@
-#ifndef RADIXSORT_H_INCLUDED
-#define RADIXSORT_H_INCLUDED
-#include <iostream>
-using namespace std;
-void radixSort(int *vetor, int tam, int *numTrocas);
-#endif
